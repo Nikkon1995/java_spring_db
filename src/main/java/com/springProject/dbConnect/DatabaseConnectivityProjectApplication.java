8c7a27e729e99a7fa2fabdcf8863d7ae38b6c043
Nikkon1995 <dutta.nikkon@gmail.com>
2024-05-14 14:45:13 +0530

    Initial Commit

---
+++ src/main/java/com/springProject/dbConnect/DatabaseConnectivityProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springProject.dbConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseConnectivityProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseConnectivityProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springProject/dbConnect/DatabaseConnectivityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springProject.dbConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseConnectivityProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
